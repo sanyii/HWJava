be9b17c4bb6217cbbc0cd0d0866cf25576926b3d
sanyii <sanyii@users.noreply.github.com>
2014-03-27 17:27:08 -0500

    Updated indentation and spacing.
    This first local commit had ugly spacing. Now fixed using the online editor. This is all to see the effect of git usage.

--- us/mci/calc/Add.java
+++ us/mci/calc/Add.java
@@ -1,15 +1,14 @@
 package us.mci.calc;
 
 public class Add{
-private long x;
-private long y;
+  private long x;
+  private long y;
 
-public add(long a, long b){
-x=a;
-y=b;
-}
-public long add(){
-return x+y;
-}
-
+  public add(long a, long b){
+    x = a;
+    y = b;
+  }
+  public long add(){
+    return x + y;
+  }
 }
